4d674328001b8c2325044bce8129797b740eb48c
HP <HP@Shreya-PC>
2026-01-29 17:46:57 +0530

    Practice Task..from..Telusko YT channel

---
+++ ConstructorAndSetterInjectionInSpring#5/src/main/java/com/myLearning/ConstructorAndSetterInjectionInSpring/Laptop.java
@@ -0,0 +1,10 @@
+package com.myLearning.ConstructorAndSetterInjectionInSpring;
+
+public class Laptop {
+    public Laptop() {
+    	System.out.println("Laptop Constructor");
+    }
+    public void compile() {
+    	System.out.println("Compiling...");
+    }
+}
